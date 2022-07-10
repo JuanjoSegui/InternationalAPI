115acb33f89bd6c6e3aba3f17f5e063b170c6646
Juan José Seguí Borja <seguipowers@gmail.com>
2022-07-10 16:23:04 +0200

    1InternacionalAPI

---
+++ Controllers/PostController.cs
@@ -0,0 +1,58 @@
+﻿using InternationalAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace InternationalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IStringLocalizer<PostController> _stringLocalizer;
+        private readonly IStringLocalizer<SharedResource> _sharedResourceLocalizer;
+
+
+        public PostController(IStringLocalizer<PostController> stringLocalizer, IStringLocalizer<SharedResource> sharedResourceLocalizer)
+        {
+            _stringLocalizer = stringLocalizer;
+            _sharedResourceLocalizer = sharedResourceLocalizer;
+        }
+
+        [HttpGet]
+        [Route("PostControllerResource")]
+        public IActionResult GetUsingPostControllerResource()
+        {
+            var article = _stringLocalizer["Article"];
+            var postName = _stringLocalizer.GetString("Wellcome").Value ?? String.Empty;
+
+            return Ok(new
+            {
+                PostType = article.Value,
+                PostName = postName,
+            });
+
+        }
+
+        [HttpGet]
+        [Route("SharedResource")]
+        public IActionResult GetUsingSharedResource()
+        {
+
+            var article = _stringLocalizer["Article"];
+            var postName = _stringLocalizer.GetString("Wellcome").Value ?? String.Empty;
+            var todayIs = string.Format(_sharedResourceLocalizer.GetString("TodayIs"), DateTime.Now.ToLongDateString());
+
+
+            return Ok(new
+            {
+                PostType = article.Value,
+                PostName = postName,
+                TodayIs = todayIs,
+            });
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,12 +2,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+//Localization
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+// Supported Cultures
+
+var supportedCultures = new[] { "en-US", "es-ES", "fr-FR" }; //USA English, European Spanish, European French
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])    //USA English default
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+    
+//Add Localization to App    
+    app.UseRequestLocalization(localizationOptions);
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
